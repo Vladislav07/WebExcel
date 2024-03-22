2405afb47b869f7e730c8ff396d771edb4bb2602
Vladislav07 <belov_vlad@list.ru>
2024-03-22 09:43:37 +0300

    init

---
+++ edrawings-api/EDrawingsUserControl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+using eDrawings.Interop.EModelViewControl;
+
+namespace edrawings_api
+{
+  
+    public partial class EDrawingsUserControl : UserControl
+    {
+        public event Action<EModelViewControl> EDrawingsControlLoaded;
+
+        public EDrawingsUserControl()
+        {
+            InitializeComponent();
+        }
+
+        public void LoadEDrawings()
+        {
+            var host = new eDrawingHost();
+            host.ControlLoaded += OnControlLoaded;
+            this.Controls.Add(host);
+            host.Dock = DockStyle.Fill;
+        }
+
+        private void OnControlLoaded(EModelViewControl ctrl)
+        {
+            EDrawingsControlLoaded?.Invoke(ctrl);
+        }
+    }
+    
+}
